bdd98c6693d8a58d25748ec6f58826de1d565616
Tony He <tony.hlp@hotmail.com>
2022-12-07 17:17:17 -0500

    feat: dockerize the app

--- func/sql_conn.php
+++ func/sql_conn.php
@@ -1,10 +1,10 @@
 <?php
 !defined('INIT_PHPV') && die('非法操作！请与管理员联系!');
 
-$urlllll = 'xxx';
-$userrrrr = 'xxx';
-$passwddddd = 'xxx';
-$con = mysqli_connect($urlllll,$userrrrr,$passwddddd,'xxx',0000);
+$urlllll = 'sql.freedb.tech';
+$userrrrr = 'freedb_olog-db-user';
+$passwddddd = '!56%UvWCaVFXvN%';
+$con = mysqli_connect($urlllll, $userrrrr, $passwddddd, 'freedb_olog-db', 3306);
 
 
 
