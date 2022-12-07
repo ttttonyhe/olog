0ff76d93ae208ff361a42c4bb443ffdc2411a1a1
Tony He <tony.hlp@hotmail.com>
2022-12-07 18:16:26 -0500

    fix: getenv

--- func/sql_conn.php
+++ func/sql_conn.php
@@ -3,7 +3,7 @@
 
 $con = mysqli_init();
 $con->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-$con->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+$con->real_connect(getenv("HOST"), getenv("USERNAME"), getenv("PASSWORD"), getenv("DATABASE"));
 
 
 ?>
